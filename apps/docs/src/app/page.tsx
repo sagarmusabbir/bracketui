1c395c554e8e09c82a278bd828a83a95cade5e9b
sagarmusabbir <sagarmusabbir@gmail.com>
2024-12-25 00:47:45 +0600

    first commit

---
+++ apps/docs/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@thirdbracket/core";
+
+export default function Page() {
+  return (
+    <>
+      <h1>Web</h1>
+      <Button>Boop</Button>
+    </>
+  );
+}
---
+++ packages/thirdbracket-core/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ["@thirdbracket/eslint-config/react-internal.js"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+};
