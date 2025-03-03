626da905c4197aa04d0ca33342d223b30466505a
sagarmusabbir <sagarmusabbir@gmail.com>
2025-03-04 02:39:31 +0600

    flowbite-sections

--- apps/docs/src/app/tb/page.tsx
+++ apps/docs/src/app/tb/page.tsx
@@ -4,12 +4,30 @@ import { Bracket } from "@thirdbracket/bracketui";
 
 import { DefaultHero } from "../../../components/Hero/default";
 import Subscription from "../../../components/Newsletter/Subscription";
+import { FeatureListCardsSection } from "../../../components/Feature/card-list";
+import { AlternateImageWithFeatureListSection } from "../../../components/Feature/alternate";
+import { ComparisonCardsFeatureSection } from "../../../components/Feature/comparison";
+import { FeatureListWithCTAsSection } from "../../../components/Feature/cta-list";
+import { DescriptionWithFeatureListIconsSection } from "../../../components/Feature/description-icon-list";
+import { FeatureIconsAndCTASection } from "../../../components/Feature/icon-list-cta";
+import { FeatureListWithIconsSection } from "../../../components/Feature/icons-list";
+import { ImageWithFeatureListAndCTAsSection } from "../../../components/Feature/image-list";
+import { RoundedIconsFeatureSection } from "../../../components/Feature/rounded-icons";
 
 export default function TBPage() {
   return (
     <main className="bg-white dark:bg-gray-950 text-gray-950 dark:text-white">
       <Bracket>
         <DefaultHero />
+        <AlternateImageWithFeatureListSection />
+        <FeatureListCardsSection />
+        <ComparisonCardsFeatureSection />
+        <FeatureListWithCTAsSection />
+        <DescriptionWithFeatureListIconsSection />
+        <FeatureIconsAndCTASection />
+        <FeatureListWithIconsSection />
+        <ImageWithFeatureListAndCTAsSection />
+        <RoundedIconsFeatureSection />
         <Subscription />
       </Bracket>
     </main>
